e3867ed293280b40720ae3313c775c3b174ad89e
Bera <burakekici44@gmail.com>
2019-11-22 10:43:04 +0300

    Yüz tanıma sistemi yapıldı.

--- FaceRecognitionApp/ViewController.swift
+++ FaceRecognitionApp/ViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signInClicked(_ sender: Any) {
+        let authContext = LAContext()
+        var err: NSError?
+        if authContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &err){
+            
+            authContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Is it you?") { (success, error) in
+                if success {
+                    //Bu şekilde yapıldığı takdirde segue olayını veya herhangi bir olayı arka planda çalıştırmaya çalıştığından program hata veriyor. Bu değişiklikleri başka bir thread içerisinde yapmak gerekiyor.
+                    //------>> self.performSegue(withIdentifier: "singInVC", sender: nil)
+                    
+                    //Uyguladığımız fonksiyonu DisPatchQueue sınıfını kullanarak istediğimiz yerde async şekilde çalışıtırabiliyoruz.
+                    DispatchQueue.main.async {
+                        self.performSegue(withIdentifier: "signInVC", sender: nil)
+                    }
+                }
+                else{
+                    DispatchQueue.main.async {
+                        self.myLabel.text = "Error : \(String(describing: error))"
+                    }
+                    
+                }
+            }
+        }
+    }
+    
 }
 
