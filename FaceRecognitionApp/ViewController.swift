64df2d82c10dc018141364a1dc841011b34a2f51
Bera <burakekici44@gmail.com>
2019-11-22 10:20:17 +0300

    Initial Commit

---
+++ FaceRecognitionApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceRecognitionApp
+//
+//  Created by Bera on 22.11.2019.
+//  Copyright © 2019 Bera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
